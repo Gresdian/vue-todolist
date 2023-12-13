7956f22afa32715abe658e6fb4db757382a4a81c
Gresdian <chris_da98@outlook.it>
2023-12-13 18:56:29 +0100

    MILESTONE1 complete

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            tasks : [
+                {
+                    text: 'Preparare la valigia',
+                    done: true,
+                },
+                {
+                    text: 'Stampare biglietto',
+                    done: true,
+                },
+                {
+                    text: 'Portare scarpe comode',
+                    done: true,
+                },
+                {
+                    text: 'Ricordarsi lo spazzolino',
+                    done: true,
+                },
+                {
+                    text: 'Comprare crema solare',
+                    done: true,
+                },
+            ]
+        }
+    },
+    methods:{
+        toggleDone(index){
+            this.tasks[index].done = !this.tasks[index].done;
+        }
+    },
+}).mount('#app');
